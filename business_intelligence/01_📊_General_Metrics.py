627ff2cd37c203ee9dd6aed785d3d3b89b567fcf
Cristhian <crijpardomer@unal.edu.co>
2023-08-02 19:39:45 -0500

    aading pages

--- business_intelligence/01_📊_General_Metrics.py
+++ business_intelligence/01_📊_General_Metrics.py
@@ -1,10 +1,13 @@
 import streamlit as st
 import pandas as pd
+import utils
 from utils.load_data import load_multiple_data
 from operator import itemgetter
 import altair as alt
 
-st.set_page_config(page_title="Timeseries", page_icon="")
+
+page_title="General Metrics"
+st.set_page_config(page_title, page_icon="")
 data_list = [
     {"share": "share__products_bi", "schema": "platinum", "table": "reviews_count_per_day"}
 ]
@@ -14,5 +17,5 @@ data = load_multiple_data(data_list)
     reviews_count_per_day
 ) = itemgetter("reviews_count_per_day")(data)
 
-st.title("Time series")
+st.title(page_title)
 st.dataframe(reviews_count_per_day)
