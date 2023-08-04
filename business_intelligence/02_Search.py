66c4a97092bd9449287d64723957d3492dd50bc4
Juanchobanano <juancepeda.gestion@gmail.com>
2023-08-04 16:36:53 -0500

    changes

--- business_intelligence/02_Search.py
+++ business_intelligence/02_Search.py
@@ -8,71 +8,74 @@ import altair as alt
 from streamlit_agraph import agraph, Node, Edge, Config, ConfigBuilder
 import constants as ct
 
+# Page title
 st.set_page_config(page_title="Products Search", page_icon="")
-products_graph = pd.read_parquet("graph.parquet")
-amazon_reviews = pd.read_parquet("amazon_reviews.parquet")
-amazon_metadata = pd.read_parquet("amazon_metadata.parquet")
-products_graph.columns = ["src", "dst", "label", "weight"]
 
+# Load parquets
+products_graph = pd.read_parquet("./data/graph.parquet")
+amazon_reviews = pd.read_parquet("./data/amazon_reviews.parquet")
+amazon_metadata = pd.read_parquet("./data/amazon_metadata.parquet")
+
+# Main title
 st.title("🔍 Products Search")
 
+product_title = st.text_input("Search products by title", value = "Nintendo 64")
+
 # Searh bar
-
-product = st.text_input("Search products by title", value = "Nintendo 64")
-
 product = "B00005YDIC"
-
 amazon_metadata = amazon_metadata[amazon_metadata.asin == product]
 amazon_reviews = amazon_reviews[amazon_reviews.asin == product]
+
+# Product Information
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.subheader("🏷️ Title")
+    st.markdown(f"#### {product_title}")
+with col2: 
+    st.subheader("🏢 Brand")
+    brand = amazon_metadata["brand"].values[0]
+    st.markdown(f"#### {brand}")
+with col3: 
+    st.subheader("📂 Category")
+    category = amazon_metadata["main_category"].values[0]
+    st.markdown(f"#### {category}")
+
+# Show additional product information
+st.subheader("ℹ️ Additional Product Information")
+additional_info = amazon_metadata[["description", "feature", "category", "mean_price"]]
+st.dataframe(additional_info)
+
+# Show product reviews
+st.subheader("📝 Product Reviews")
+show_amazon_reviews = amazon_reviews[["reviewID", "date", "reviewerName", "summary", "reviewText", "verified", "vote"]]
+st.dataframe(show_amazon_reviews)
 # Filter the dataframe using masks
 #m1 = df["Autor"].str.contains(text_search)
 #m2 = df["Título"].str.contains(text_search)
 #df_search = df[m1 | m2]
-st.dataframe(amazon_metadata)
-st.dataframe(amazon_reviews)
 
-#----
-st.markdown("## Nintendo 64")
+# Show product subgraph
+st.subheader("🏘️ Product Neighborhood")
+
+# Show degree and betweenness centrality metrics
 col1, col2 = st.columns(2)
 with col1: 
-    st.metric(label = "Degree Centrality", value = 0.7)
+    st.metric(label = "Product Local Degree Centrality (from 0 to 1)", value = 0.7)
+    st.caption("Measure how popular a product is, helping companies to allocate marketing budgets more effectively and focus on products with high customer engagement.")
 with col2: 
-    st.metric(label = "Betweenness Centrality", value = 0.6)
+    st.metric(label = "Product Local Betweenness Centrality (from 0 to 1)", value = 0.6)
+    st.caption("Measures how influential a product is, helping companies target their marketing efforts more effectively. Collaborating with influential brands or featuring influential products can boost exposure and sales.")
 
-#----
-
-
-
-
+# Get graph
 g1 = products_graph[products_graph['src'] == product]
 result = pd.merge(g1[['dst']], products_graph[['src', 'dst', 'label', 'weight']], left_on='dst', right_on='dst', suffixes=('', '_g2'))
-# Step 3: Join g2 and g3
-#result = pd.merge(result[['src', 'dst', 'label', 'weight']], products_graph[['src', 'dst', 'label', 'weight']], left_on='src', right_on='src', suffixes=('_g2', ''))
-
-# Select the desired columns from the final result
 result = result[['src', 'dst', 'label', 'weight']]
-#result = result[result.label != 'HAS_REVIEW']
-#result = result[result.label != 'HAS_CATEGORY']
-
 main = result[(result.src == product) | (result.label == 'HAS_REVIEW')]# & (result.src == product))]
 not_main = result[(result.src != product) & (result.label != 'HAS_REVIEW')]
 not_main = not_main.sample(n = 200)
-
-#print("length main: ", len(main))
-#print("length not main: ", len(not_main))
-
 result = pd.concat([main, not_main])
-
-#print("Length result: ", len(result))
-
-
-
-# Display the final result
 communities__graph = result
-
-#communities__graph = communities__graph[communities__graph.community_id == community_number]
 src = communities__graph["src"].to_frame()
-
 src["label"] = "asin"
 dst = communities__graph["dst"].to_frame()
 dst["label"] = communities__graph["label"].apply(lambda z: ct.dicc[z])
@@ -80,8 +83,7 @@ dst.columns = ["src", "label"]
 nodes = pd.concat([src, dst]).reset_index(drop = True)
 nodes = nodes.drop_duplicates(subset=["src"])
 
-#print(list(communities__graph.itertuples())[0:4])
-
+# Prepare graph.
 nodes_results = list()
 for row in nodes.itertuples():
     if row.src == product:
@@ -101,7 +103,6 @@ for row in nodes.itertuples():
         node = Node(id = row.src, title = row.src, image = ct.node_image[row.label], shape = "circularImage", size = ct.size_image[row.label])
     nodes_results.append(node)
 
-#nodes = [Node(id = row.src, title = row.src, image = ct.node_image[row.label], shape = "circularImage", size = ct.size_image[row.label]) for row in nodes.itertuples()] 
 edges = [Edge(source = row.src, label = row.label, target = row.dst, font = {"size": ct.edge_font_size[row.label]}) for row in communities__graph.itertuples()]
 
 config = Config(
@@ -120,9 +121,7 @@ config = Config(
                 solver = "forceAtlas2Based",
                 )
 
+# Show graph.
 return_value = agraph(nodes=nodes_results, 
                     edges=edges, 
                     config=config)
-
-print(return_value)
-
