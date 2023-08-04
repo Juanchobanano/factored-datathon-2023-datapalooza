e7d74b6a116aebeddc6d6ead003ad5d52cfe53a8
eguar11011 <emenedezc@unal.edu.co>
2023-08-04 11:30:15 -0500

    algunas grficas con plotly

--- business_intelligence/01_📊_General_Metrics.py
+++ business_intelligence/01_📊_General_Metrics.py
@@ -5,9 +5,16 @@ from utils.load_data import load_multiple_data
 from operator import itemgetter
 import altair as alt
 
+import plotly.express as px
+import plotly.graph_objects as go
+
 
 page_title="General Metrics"
-st.set_page_config(page_title, page_icon="")
+st.set_page_config(
+    page_title, 
+    page_icon="",
+    layout= "wide")
+
 data_list = [
     {"share": "share__hackaton__streamlit", "schema": "platinum", "table": "avg_words_per_review"},
     {'share': 'share__hackaton__streamlit', 'schema': 'platinum', 'table': 'count_reviews_per_category'},
@@ -70,6 +77,74 @@ data = load_multiple_data(data_list)
     "verified_distribution",
     "vote_distribution"
     )(data)
+
+
+
+#Valores de la cabeza del proyecto
+#total_products_per_category
+#count_similar_products_between_categories
+#count_verified_reviews_per_category
+#count_reviews_per_category
+
+st.title("Discover the Magical Data Visualization! An Experience That Will Leave You Speechless!")
+
+
+    # create three columns
+kpi1, kpi2, kpi3, kpi4 = st.columns(4)
+
+# fill in those three columns with respective metrics or KPIs
+kpi1.metric(
+    label="total products per category",
+    value=f"{total_products_per_category['total_products'][0] , total_products_per_category['total_products'][1]}",
+    delta= 5,
+)
+
+kpi2.metric(
+    label="Married Count 💍",
+    value=12,
+    delta=66,
+)
+
+kpi3.metric(
+    label="A/C Balance ＄",
+    value=5 ,
+    delta=7,
+)
+
+kpi4.metric(
+    label="A/C Balance ＄",
+    value=5 ,
+    delta=7,
+)
+# Crear la figura con Plotly
+fig_col1, fig_col2 = st.columns(2)
+
+with fig_col1:
+    st.markdown("### First Chart")
+    fig1 = go.Figure(data=[
+        go.Bar(name='Overall 1', x=count_reviews_per_category_and_overall[count_reviews_per_category_and_overall['overall'] == 1]['category'], y=count_reviews_per_category_and_overall[count_reviews_per_category_and_overall['overall'] == 1]['count']),
+        go.Bar(name='Overall 2', x=count_reviews_per_category_and_overall[count_reviews_per_category_and_overall['overall'] == 2]['category'], y=count_reviews_per_category_and_overall[count_reviews_per_category_and_overall['overall'] == 2]['count']),
+        go.Bar(name='Overall 3', x=count_reviews_per_category_and_overall[count_reviews_per_category_and_overall['overall'] == 3]['category'], y=count_reviews_per_category_and_overall[count_reviews_per_category_and_overall['overall'] == 3]['count']),
+        go.Bar(name='Overall 4', x=count_reviews_per_category_and_overall[count_reviews_per_category_and_overall['overall'] == 4]['category'], y=count_reviews_per_category_and_overall[count_reviews_per_category_and_overall['overall'] == 4]['count']),
+        go.Bar(name='Overall 5', x=count_reviews_per_category_and_overall[count_reviews_per_category_and_overall['overall'] == 5]['category'], y=count_reviews_per_category_and_overall[count_reviews_per_category_and_overall['overall'] == 5]['count']),
+    ])
+
+    # Actualizar el diseño del gráfico
+    fig1.update_layout(barmode='group', xaxis_title='Categoría', yaxis_title='Conteo',
+                    title='Conteo por Categoría y Overall', legend_title='Overall')
+
+    # Mostrar el gráfico en Streamlit
+    st.write(fig1)
+
+with fig_col2:
+    st.markdown("### Second Chart")
+    fig2 = px.bar(count_reviews_per_category_and_overall, x='count', y='category', orientation='h', color='overall', 
+                labels={'count': 'Conteo', 'category': 'Categoría'},
+                title='Conteo por Categoría (Inverso)',
+                height=500, color_discrete_sequence=px.colors.qualitative.Plotly[::-1])
+    st.write(fig2)
+
+
 
 st.title(page_title)
 st.text("avg_words_per_review")
