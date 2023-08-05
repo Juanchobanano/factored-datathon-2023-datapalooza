ef59ad64a04dfee38c8617270b44c95bb6a23508
Cristhian <crijpardomer@unal.edu.co>
2023-08-05 18:06:47 -0500

    adding view of real time metrics

--- business_intelligence/pages/03_📈_Real_Time_Analytics.py
+++ business_intelligence/pages/03_📈_Real_Time_Analytics.py
@@ -20,15 +20,23 @@ index = pinecone.Index(ct.PINECONE_INDEX_NAME)
 data_list = [
     {"share": "share__hackaton__streamlit", "schema": "platinum", "table": "streaming_reviews_kpi_1"},
     {"share": "share__hackaton__streamlit", "schema": "platinum", "table": "streaming_reviews_kpi_2"},
+    {"share": "share__hackaton__streamlit", "schema": "silver", "table": "amazon_metadata_silver_selected"},
+    {"share": "share__hackaton__streamlit", "schema": "platinum", "table": "amazon_reviews_selected_with_intention_and_sentiment"},
 
 ]
 data = load_multiple_data(data_list)
 (
     streaming_reviews_kpi_1,
-    streaming_reviews_kpi_2
+    streaming_reviews_kpi_2,
+    products_metadata,
+    amazon_reviews_selected_with_intention_and_sentiment
+ 
 ) = itemgetter(
     "streaming_reviews_kpi_1",
-    "streaming_reviews_kpi_2"
+    "streaming_reviews_kpi_2",
+    "amazon_metadata_silver_selected",
+    "amazon_reviews_selected_with_intention_and_sentiment"
+ 
 )(data)
 
 # main title
@@ -37,26 +45,39 @@ st.title("📈 Real Time Analytics")
 col1, col2 = st.columns(2)
 with col1:
     st.subheader("💪 Factored EventHub")
+    st.divider()
     st.metric(
-    label="Total Factored EventHub Streaming Data",
-    value=10,
-    delta= 5)
-    st.line_chart(streaming_reviews_kpi_1)
+    label="Total Factored EventHub Unique Streaming Data",
+    value=59954,
+    delta= f"{round(100*(59954-48306)/59954,2)}%" #TODO: sacarlos los totales de los kpis
+    )
+    # st.divider()
+    st.line_chart(streaming_reviews_kpi_1, x="day")
 with col2:
     st.subheader("🚀 Datalaooza EventHub")
+    st.divider()
+    # st.dataframe(streaming_reviews_kpi_2)
     st.metric(
     label="Total Datapalooza EventHub Streaming Data",
-    value=10,
-    delta= 5)
-    st.line_chart(streaming_reviews_kpi_2)
+    value=6141,
+    delta= f"{round(100*(6141-6098)/6141,2)}%" #TODO: sacarlos los totales de los kpis
+    )
+    # st.divider()
+    st.line_chart(streaming_reviews_kpi_2, x="date")
 
 
 st.divider()
 
 st.markdown("Utilize semantic search to find products effortlessly. Explore Intentions and Semtiment on streaming captured data!🚀")
-st.caption("Start by trying something like 'Nintendo 64'")
+st.caption("Start by trying something like 'Nintendo Entertainment System'")
 query_text = st.text_input("🛍️ Search a product")
-
+max_results = st.slider(
+    "🎛️ Select the maximum number of elements you want to retrieve", 
+    min_value = 5, 
+    max_value = 30, 
+    step = 5, 
+    key = "slider_1"
+)
 
 
 # If query text not empty
@@ -68,10 +89,25 @@ if query_text:
     results_asin = [x["id"] for x in xc["matches"]] # Get ids of asins
 
     # Filter results metadata and reviews
-    results_asin_mask = product_metadata["asin"].isin(results_asin)
-    results_metadata = product_metadata[results_asin_mask].reset_index(drop=True).fillna("Not available")
+    results_asin_mask = products_metadata["asin"].isin(results_asin)
+    results_metadata = products_metadata[results_asin_mask].reset_index(drop=True).fillna("Not available")
 
     # Display query results
     options = list(results_metadata["title"].values)
     product_idx = st.selectbox("📊 Query results", range(len(options)), format_func=lambda x: options[x])
-  
+    asin = results_asin[product_idx]
+
+    filtered_df = amazon_reviews_selected_with_intention_and_sentiment[amazon_reviews_selected_with_intention_and_sentiment.asin == asin]
+    bool_ = (len(filtered_df) == 0)
+    print(bool_)
+
+    #check if product is in streaming data
+    st.markdown(f"The product {options[product_idx]} has reviews from streaming data?")
+    if bool_:
+        st.markdown("NO")
+    else:
+        st.markdown("Yes")
+        
+
+    if not bool_:
+        st.dataframe(filtered_df)       
